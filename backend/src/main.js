3e67880c2d11755f144d350a63dd65cb73df3865
csandrisTurr <csaszarandras@turr.hu>
2024-10-16 10:55:46 +0200

    extended urlencoded

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 const port = 3000;
 
