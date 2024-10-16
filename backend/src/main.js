5b44181edbb3181db6ef0a902cedc5b4adc65a6a
csandrisTurr <csaszarandras@turr.hu>
2024-10-16 08:59:26 +0200

    mysql dolgok

--- backend/src/main.js
+++ backend/src/main.js
@@ -2,7 +2,39 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let mysql = require('mysql');
+let connection;
+
+// valamiert nem mukodik a try de akkor Rendben van.
+connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: '13a_konyvtar',
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    console.log('nem tudtam kapcsolodni az adatbazisra testver');
+    require('process').exit(1);
+  }
+});
+
+// globalis error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('valami nem jo szerintem');
+});
+
 app.get('/books', (req, res) => {
+  connection.query(
+    'SELECT 1 + 1 AS solution',
+    function (error, results, fields) {
+      if (error) throw error;
+    }
+  );
+
   res.send('Hello World!');
 });
 
